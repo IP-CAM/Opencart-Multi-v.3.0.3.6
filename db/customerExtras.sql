7b1cd3cb04f122a51c7d404fe95ce8659db3ef63
Samuel Fujimoto <fujimoto.samuel@gmail.com>
2020-11-23 17:37:47 -0300

    add: Address and Customer dependencies tables SSO DB

---
+++ db/customerExtras.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE IF NOT EXISTS `sso`;
+
+USE `sso`;
+
+--
+-- Table structure for table `oc_customer_ip`
+--
+
+DROP TABLE IF EXISTS `oc_customer_ip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_customer_ip` (
+  `customer_ip_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `ip` varchar(40) NOT NULL,
+  `date_added` datetime NOT NULL,
+  PRIMARY KEY (`customer_ip_id`),
+  KEY `ip` (`ip`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_customer_ip`
+--
+
+LOCK TABLES `oc_customer_ip` WRITE;
+/*!40000 ALTER TABLE `oc_customer_ip` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_customer_ip` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Table structure for table `oc_customer_login`
+--
+
+DROP TABLE IF EXISTS `oc_customer_login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_customer_login` (
+  `customer_login_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(96) NOT NULL,
+  `ip` varchar(40) NOT NULL,
+  `total` int(4) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_modified` datetime NOT NULL,
+  PRIMARY KEY (`customer_login_id`),
+  KEY `email` (`email`),
+  KEY `ip` (`ip`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_customer_login`
+--
+
+LOCK TABLES `oc_customer_login` WRITE;
+/*!40000 ALTER TABLE `oc_customer_login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_customer_login` ENABLE KEYS */;
+UNLOCK TABLES;
