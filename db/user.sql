a47d80a08f2684d0e4ccdfefe9e212e0a39d8b91
Samuel Fujimoto <fujimoto.samuel@gmail.com>
2020-11-24 09:50:48 -0300

    db: add user for opencart admin

--- db/user.sql
+++ db/user.sql
@@ -43,6 +43,8 @@ ADD COLUMN `store` VARCHAR(45) NULL AFTER `date_added`;
 
 UPDATE `sso`.`oc_user` SET `store` = 'sws' WHERE (`user_id` = '1');
 
+INSERT INTO `oc_user` VALUES (2,1,'sso','3cdda6204299e80712cedcb80423c08036f044e9','EFrk3mfAv','John','Doe','sws.samuel@gmail.com','','','',1,'2020-11-17 11:41:08','opencart');
+
 --
 -- Table structure for table `oc_user_group`
 --
