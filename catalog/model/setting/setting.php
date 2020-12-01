5a0eb03074e108c255d2816cde4fb579d9b5e92c
Samuel Fujimoto <fujimoto.samuel@gmail.com>
2020-12-01 17:44:30 -0300

    feature: adaptacao do store SSO para o config do opencart

--- catalog/model/setting/setting.php
+++ catalog/model/setting/setting.php
@@ -13,10 +13,27 @@ class ModelSettingSetting extends Model {
 			}
 		}
 
+		if ($code == 'config'){
+			$ssoquery = $this->ssodb->query("SELECT * FROM store WHERE store = '". DB_DATABASE ."'");
+			$storeInfo = sso\store::storeToConfig($ssoquery->row);
+			foreach ($storeInfo as $key => $info) {
+				$data[$key] =  $info;
+			}
+		}
+
 		return $data;
 	}
 	
 	public function getSettingValue($key, $store_id = 0) {
+		
+		if (sso\store::checkConfigStoreSSO($key)){
+			$ssoquery = $this->ssodb->query("SELECT * FROM store WHERE store = '". DB_DATABASE ."'");
+			$storeInfo = sso\store::storeToConfig($ssoquery->row);
+			if (isset($storeInfo[$key])){
+				return $storeInfo[$key];
+			}
+		}
+
 		$query = $this->db->query("SELECT value FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `key` = '" . $this->db->escape($key) . "'");
 
 		if ($query->num_rows) {
